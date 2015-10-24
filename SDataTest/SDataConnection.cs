d53b62ccaf1af54ea8ba0e45db722a8c08e7ac01
gokuger <michael.koch@hochscherf.com>
2015-10-24 21:57:04 +0200

    2015-10-24_2156
    Initial

---
+++ SDataTest/SDataConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDataTest
+{
+    class SDataConnection
+    {
+        private String connectionPath = "";
+        private String user = "";
+        private String password = "";
+        
+        public SDataConnection(String connectionPath, String user, String password)
+        {
+            this.connectionPath = connectionPath;
+            this.user = user;
+            this.password = password;
+        }
+
+        public void connect()
+        {
+ 
+        }
+    }
+}
