3eaf967f899d3140a3bcb0d870d1d93e6f71b896
gokuger <michael.koch@hochscherf.com>
2015-10-24 22:16:55 +0200

    2015-10-24_2216

--- SDataTest/SDataConnection.cs
+++ SDataTest/SDataConnection.cs
@@ -3,25 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sage.SData.Client.Core;
+using Sage.SData.Client.Atom;
+using Sage.SData.Client.Extensions;
 
 namespace SDataTest
 {
     class SDataConnection
     {
-        private String connectionPath = "";
+        private String url = "";
         private String user = "";
         private String password = "";
+        private String ressourceKind;
         
-        public SDataConnection(String connectionPath, String user, String password)
+        public SDataConnection(String url, String user, String password, String ressourceKind)
         {
-            this.connectionPath = connectionPath;
+            this.url = url;
             this.user = user;
             this.password = password;
+            this.ressourceKind = ressourceKind;
         }
 
-        public void connect()
+        public void setConnection()
         {
- 
+            ISDataService service = new SDataService(url, user, password);
+            var request = new SDataResourceCollectionRequest(service);
+        }
+
+        public void setRessourceKind(String ressourceKind)
+        {
+            
         }
     }
 }
